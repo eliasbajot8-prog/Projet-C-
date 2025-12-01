58a30f545ed5f2593387f92a817103cdd905f53f
Elias Bajot-Angeloski <eliasbajotangeloski@MacBook-Air-de-Elias.local>
2025-12-01 13:50:20 +0100

    Modification de rhs pour ameliorer la courbe de debit

--- SRC/windkessel.cpp
+++ SRC/windkessel.cpp
@@ -26,17 +26,30 @@ double WindkesselModel::compliance(double P) const
     return p.getC0() / (1.0 + p.getAlpha() * P);
 }
 
+// Dans WindkesselModel::rhs
+
 P_Q_System WindkesselModel::rhs(double t, const P_Q_System &X) const
 {
     P_Q_System dX;
 
     double C = compliance(X.P);
 
-    // 1) Conservation au noeud : C dP/dt = Qin - P/Rp
+    // Équation de pression (inchangée)
     dX.P = (Qin - X.P / p.getRp()) / C;
 
-    // 2) Inertance : Lw dQ/dt = Pin - P - Rc * Q
-    dX.Q = (Pin(t) - X.P - p.getRc() * X.Q) / p.getLw();
+    // --- AJOUT DE LA LOGIQUE DE VALVE ---
+    double P_ventricule = Pin(t);
+    double R_total = p.getRc(); // Résistance de base
+
+    if (P_ventricule < X.P)
+    {
+        // On multiplie la résistance par un grand facteur (ex: 1000)
+        // Cela va "étouffer" le débit négatif très rapidement
+        R_total *= 1000.0;
+    }
+
+    // Équation de débit avec la résistance conditionnelle
+    dX.Q = (P_ventricule - X.P - R_total * X.Q) / p.getLw();
 
     return dX;
 }
--- plot_windkessel.py
+++ plot_windkessel.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # 1. Chargement des données "Couplé" (Le système complet)
 try:
-    df_P_c = pd.read_csv('data/P_wk_coupled.csv', header=None, names=['t', 'val'])
-    df_Q_c = pd.read_csv('data/Q_wk_coupled.csv', header=None, names=['t', 'val'])
+    df_P_c = pd.read_csv('data/P_wk_alone.csv', header=None, names=['t', 'val'])
+    df_Q_c = pd.read_csv('data/Q_wk_alone.csv', header=None, names=['t', 'val'])
     
 except FileNotFoundError:
     print("Erreur : Fichiers CSV manquants. Lance './solver parameters.in' d'abord.")
@@ -15,18 +15,18 @@ plt.figure(figsize=(12, 10))
 # --- Graphique Pression ---
 plt.subplot(2, 1, 1)
 # Courbe Couplée (Venant de l'artère)
-plt.plot(df_P_c['t'], df_P_c['val'] / 133.322, label='Modèle Couplé (Artère)', color='blue', linewidth=2)
+plt.plot(df_P_c['t'], df_P_c['val'] / 133.322, label='Modèle Windkessel seul (ODE)', color='blue', linewidth=2)
 
 
 plt.ylabel('Pression (mmHg)')
-plt.title('Pression et débit dans l artère avec modèle Windkessel couplé (PDE + ODE)')
+plt.title('Pression et débit dans l artère avec modèle Windkessel seul (ODE)')
 plt.legend()
 plt.grid(True, alpha=0.5)
 
 # --- Graphique Débit ---
 plt.subplot(2, 1, 2)
 # Débit sortant de l'artère
-plt.plot(df_Q_c['t'], df_Q_c['val'] * 1e6, label='Débit Couplé (Sortie PDE)', color='blue', linewidth=2)
+plt.plot(df_Q_c['t'], df_Q_c['val'] * 1e6, label='Débit Windkessel seul (ODE)', color='blue', linewidth=2)
 
 plt.ylabel('Débit (mL/s)')
 plt.xlabel('Temps (s)')
@@ -34,5 +34,5 @@ plt.legend()
 plt.grid(True, alpha=0.5)
 
 plt.tight_layout()
-plt.savefig('Couplés.png', dpi=300)
+plt.savefig('WK.png', dpi=300)
 plt.show()
